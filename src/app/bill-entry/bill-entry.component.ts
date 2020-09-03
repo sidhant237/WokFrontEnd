a55e4979317c29be7ff5e4ac638512f91eea8f38
joshyjoy <joshy.joy@cognicor.com>
2020-09-04 01:20:51 +0530

    bill entry 75% completed

--- src/app/bill-entry/bill-entry.component.ts
+++ src/app/bill-entry/bill-entry.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from './../../environments/environment';
 
 @Component({
   selector: 'app-bill-entry',
@@ -7,9 +10,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BillEntryComponent implements OnInit {
 
-  constructor() { }
+  date: any;
+  outlet: string;
+  employee: string;
+  supplier: string;
+  payment: string;
+
+  itemsData: any;
+  billData: any = [];
+
+  updatingStock = false;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.date = new Date().toISOString().slice(0, 10);
+    this.http.get(environment.url + 'billentry').subscribe(
+      (result: StockData) => {
+        this.itemsData = result;
+      }, error => {
+        console.log(error);
+      }
+    );
+  }
+
+
+  stockUpdateHandler() {
+    this.updatingStock = true;
+    const updatedStock = {};
+    updatedStock['outlet'] = this.outlet;
+    updatedStock['items'] = [];
+    this.itemsData.filter(item => item.qty !== null).forEach(
+      item => {
+        updatedStock['items'].push(
+          {
+            'id': item.ID,
+            'qty': item.qty,
+            'employee': this.employee,
+            'date': this.date
+          }
+        );
+      }
+    );
+    this.http.post(environment.url + 'billentry', updatedStock).subscribe(
+      result => {
+        this.updatingStock = false;
+        console.log(result);
+      }, error => {
+        this.updatingStock = false;
+        console.log(error);
+      }
+    );
+  }
+
+  billItemInsertHandler() {
+    this.billData.push({
+      'itemName': '',
+      'qty': 0,
+      'rate': 0,
+      'amount': 0
+    });
+  }
+
+  billItemRemoveHandler(index) {
+    this.billData.splice(index, 1);
+    console.log(this.billData);
   }
 
 }
+
+export interface StockData {
+  Items: any;
+  Outlets: any;
+  Employees: any;
+  Suppliers: any;
+  PayTerms: any;
+}
+
