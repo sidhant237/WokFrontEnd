4a5065b9d6e238ae6675c029f38fc52eb5ed0020
joshyjoy <joshy.joy@cognicor.com>
2020-09-05 00:24:39 +0530

    outlet-transfer component completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { StockEntryComponent } from './stock-entry/stock-entry.component';
 import { BillEntryComponent } from './bill-entry/bill-entry.component';
+import { OutletTransferComponent } from './outlet-transfer/outlet-transfer.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'stock-entry', pathMatch: 'full' },
   { path: 'stock-entry', component: StockEntryComponent },
-  { path: 'bill-entry', component: BillEntryComponent }
+  { path: 'bill-entry', component: BillEntryComponent },
+  { path: 'outlet-transfer', component: OutletTransferComponent }
 ];
 
 @NgModule({
--- src/app/outlet-transfer/outlet-transfer.component.ts
+++ src/app/outlet-transfer/outlet-transfer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from './../../environments/environment';
 
 @Component({
   selector: 'app-outlet-transfer',
@@ -6,10 +9,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./outlet-transfer.component.css']
 })
 export class OutletTransferComponent implements OnInit {
+  date: any;
+  fromOutlet: string;
+  toOutlet: string;
+  employee: string;
 
-  constructor() { }
+  itemsData: any;
+  transferData: any = [];
+
+  transferingStock = false;
+
+  alertActive = false;
+  alertMsg: string = '';
+  alertStatus: string = '';
+
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.date = new Date().toISOString().slice(0, 10);
+    this.http.get(environment.url + 'transfer').subscribe(
+      (result: StockTransferData) => {
+        this.itemsData = result;
+      }, error => {
+        console.log(error);
+      }
+    );
   }
 
+
+  stockTransferHandler() {
+    this.transferingStock = true;
+    const stockTransferData = {};
+    stockTransferData['from_outlet'] = this.fromOutlet;
+    stockTransferData['to_outlet'] = this.toOutlet;
+    stockTransferData['items'] = [];
+    this.transferData.forEach(
+      item => {
+        stockTransferData['items'].push(
+          {
+            'item': this.itemsData.Items.filter(data => data.Item === item.itemName)[0].ID,
+            'qty': item.qty,
+            'employee': this.employee,
+            'date': this.date
+          }
+        );
+      }
+    );
+    this.http.post(environment.url + 'transfer', stockTransferData).subscribe(
+      result => {
+        this.transferingStock = false;
+        this.alertGenerateHandler('Bill Generated Successfully', 'success');
+      }, error => {
+        this.transferingStock = false;
+        console.log(error);
+        this.alertGenerateHandler('something went wrong', 'error');
+      }
+    );
+  }
+
+  ItemInsertHandler() {
+    this.transferData.push({
+      'itemName': '',
+      'qty': null
+    });
+  }
+
+  itemRemoveHandler(index) {
+    this.transferData.splice(index, 1);
+  }
+
+  alertGenerateHandler(message, status) {
+    this.alertMsg = message;
+    this.alertStatus = status;
+    this.alertActive = true;
+    setTimeout(() => {
+      this.alertActive = false;
+    }, 5000);
+  }
+
+}
+
+export interface StockTransferData {
+  Items: any;
+  Outlets: any;
+  Employees: any;
 }
